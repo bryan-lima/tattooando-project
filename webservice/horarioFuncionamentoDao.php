3e580593669f828378da3f421f96356391bf1afe
Bryan <bryanlima200@outlook.com>
2020-04-29 16:59:08 -0300

    Adicionado classes para abrir e fechar conexão com BD em horarioFuncionamentoDao.php

--- webservice/horarioFuncionamentoDao.php
+++ webservice/horarioFuncionamentoDao.php
@@ -4,7 +4,18 @@ include_once "conexao.php";
 include_once "horarioFuncionamento.php";
 
 class HorarioFuncionamento {
-    
+    public $conn;
+
+    // Abre conexão com BD
+    function __construct() {
+        $conexao = new Conexao();
+        $this->conn - $conexao->getConn();
+    }
+
+    // Fecha conexão com BD
+    function __destruct() {
+        $this->conn->close();
+    }
 }
 
 ?>
