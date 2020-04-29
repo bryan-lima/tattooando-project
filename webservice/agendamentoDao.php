fd5195dd9206e4a2bc548071c77c0bd3546a9962
Bryan <bryanlima200@outlook.com>
2020-04-29 18:00:25 -0300

    Adicionado classes para abrir e fechar conexão com BD em agendamentoDao.php

--- webservice/agendamentoDao.php
+++ webservice/agendamentoDao.php
@@ -4,7 +4,18 @@ include_once "conexao.php";
 include_once "agendamento.php";
 
 class AgendamentoDao {
+    public $conn;
 
+    // Abre conexão com BD
+    function __construct() {
+        $conexao = new Conexao();
+        $this->conn - $conexao->getConn();
+    }
+
+    // Fecha conexão com BD
+    function __destruct() {
+        $this->conn->close();
+    }
 }
 
 ?>
