56fe0e921cef1f0f7545b1f74af5e3807cca5a5f
Bryan <bryanlima200@outlook.com>
2020-04-28 21:07:13 -0300

    Adicionado classes para abrir e fechar conexão com BD em clienteDao.php

--- webservice/clienteDao.php
+++ webservice/clienteDao.php
@@ -4,6 +4,18 @@ include_once "conexao.php";
 include_once "cliente.php";
 
 class ClienteDao {
+    public $conn;
+
+    // Abre conexão com BD
+    function __construct() {
+        $conexao = new Conexao();
+        $this->conn - $conexao->getConn();
+    }
+
+    // Fecha conexão com BD
+    function __destruct() {
+        $this->conn->close();
+    }
 }
 
 ?>
