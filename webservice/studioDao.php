0fc4f49a640cfe5440cb48c8922d4bc08560527c
Bryan <bryanlima200@outlook.com>
2020-04-28 23:36:31 -0300

    Adicionado função listarTodosStudios() em studioDao.php Lista todos os registros da tabela 'studio' do BD

--- webservice/studioDao.php
+++ webservice/studioDao.php
@@ -16,6 +16,22 @@ class StudioDao {
     function __destruct() {
         $this->conn->close();
     }
+
+    // Lista todos os registros da tabela 'studio' do BD
+    function listarTodosStudios() {
+        $sql = "SELECT * FROM studio";
+        $result = $this->conn->query($sql);
+        if($result->num_rows > 0) {
+            $studios = [];
+            while($row = $result->fetch_assoc()) {
+                $studio = ["id"=>$row["id"], "tipoUsuario"=>$row["tipo_usuario"], "nome"=>$row["nome"], "cpf"=>$row["cpf"], "nascimento"=>$row["nascimento"], "telefone"=>$row["telefone"], "email"=>$row["email"], "senha"=>$row["senha"], "fkEnderecoId"=>$row["fk_endereco_id"]];
+                array_push($studios, $studio);
+            }
+            echo json_encode($studios);
+        } else {
+            echo "0 resultados encontrados";
+        }
+    }
 }
 
 ?>
