90d0d6d255b9f342b83eb0e6a3eebd226b7840f2
Bryan <bryanlima200@outlook.com>
2020-04-29 18:05:07 -0300

    Adicionado função listarTodosAgendamentos() em agendamentoDao.php Lista todos os registros da tabela 'agendamento' do BD

--- webservice/agendamentoDao.php
+++ webservice/agendamentoDao.php
@@ -16,6 +16,22 @@ class AgendamentoDao {
     function __destruct() {
         $this->conn->close();
     }
+
+    // Lista todos os registros da tabela 'agendamento' do BD
+    function listarTodosAgendamentos() {
+        $sql = "SELECT * FROM agendamento";
+        $result = $this->conn->query($sql);
+        if($result->num_rows > 0) {
+            $agendamentos = [];
+            while($row = $result->fetch_assoc()) {
+                $agendamento = ["id"=>$row["id"], "fkClienteId"=>$row["fk_cliente_id"], "fkServicoId"=>$row["fk_servico_id"], "dataAgendada"=>$row["data_agendada"], "horaAgendada"=>$row["hora_agendada"], "status"=>$row["status"]];
+                array_push($agendamentos, $agendamento);
+            }
+            echo json_encode($agendamentos);
+        } else {
+            echo "0 resultados encontrados";
+        }
+    }
 }
 
 ?>
