daba39e0f2354392f27f39236dcd93e84dbdb7aa
Bryan <bryanlima200@outlook.com>
2020-04-29 16:33:20 -0300

    Adicionado função listarTodosServicos() em servicoDao.php Lista todos os registros da tabela 'servico' do BD

--- webservice/servicoDao.php
+++ webservice/servicoDao.php
@@ -16,6 +16,22 @@ class ServicoDao {
     function __destruct() {
         $this->conn->close();
     }
+
+    // Lista todos os registros da tabela 'servico' do BD
+    function listarTodosServicos() {
+        $sql = "SELECT * FROM servico";
+        $result = $this->conn->query($sql);
+        if($result->num_rows > 0) {
+            $servicos = [];
+            while($row = $result->fetch_assoc()) {
+                $servico = ["id"=>$row["id"], "fkStudioId"=>$row["fk_studio_id"], "nome"=>$row["nome"], "descricao"=>$row["descricao"], "preco"=>$row["preco"], "tempoMedio"=>$row["tempo_medio"]];
+                array_push($servicos, $servico);
+            }
+            echo json_encode($servicos);
+        } else {
+            echo "0 resultados encontrados";
+        }
+    }
 }
 
 ?>
