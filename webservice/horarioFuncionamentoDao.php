aed03f787cf58017f55615921fcdc1d4c6539538
Bryan <bryanlima200@outlook.com>
2020-04-29 17:05:49 -0300

    Adicionado função listarTodosHorariosFuncionamento() em horarioFuncionamentoDao.php Lista todos os registros da tabela 'horario_funcionamento' do BD

--- webservice/horarioFuncionamentoDao.php
+++ webservice/horarioFuncionamentoDao.php
@@ -16,6 +16,22 @@ class HorarioFuncionamento {
     function __destruct() {
         $this->conn->close();
     }
+
+    // Lista todos os registros da tabela 'horario_funcionamento' do BD
+    function listarTodosHorariosFuncionamento() {
+        $sql = "SELECT * FROM horario_funcionamento";
+        $result = $this->conn->query($sql);
+        if($result->num_rows > 0) {
+            $horariosFuncionamento = [];
+            while($row = $result->fetch_assoc()) {
+                $horarioFuncionamento = ["id"=>$row["id"], "fkStudioId"=>$row["fk_studio_id"], "diasAberto"=>$row["dias_aberto"], "hrSemanaInicio"=>$row["hr_semana_inicio"], "hrSemanaFim"=>$row["hr_semana_fim"], "hrFimSemanaInicio"=>$row["hr_fim_semana_inicio"], "hrFimSemanaFim"=>$row["hr_fim_semana_fim"]];
+                array_push($horariosFuncionamento, $horarioFuncionamento);
+            }
+            echo json_encode($horariosFuncionamento);
+        } else {
+            echo "0 resultados encontrados";
+        }
+    }
 }
 
 ?>
