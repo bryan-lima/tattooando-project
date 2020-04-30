3bf97e478919c038ee3678cba8907b024e5b2e53
Bryan <bryanlima200@outlook.com>
2020-04-30 00:23:52 -0300

    Em horarioFuncionamentoControle.php, iniciado construção da estrutura para tratamento dos métodos de requisição HTTP com base no modelo arquitetural REST (GET, POST, PUT e DELETE)

--- webservice/horarioFuncionamentoControle.php
+++ webservice/horarioFuncionamentoControle.php
@@ -4,4 +4,18 @@ include_once "model/horarioFuncionamento.php";
 include_once "dao/horarioFuncionamentoDao.php";
 header('Content-Type: application/json; charset=utf-8');
 
+// Captura o método de requisição e armazena na váriavel
+$metodo = $_SERVER['REQUEST_METHOD'];
+
+// Modelo arquitetural REST
+if($metodo == "GET") {
+
+} else if($metodo == "POST") {
+
+} else if($metodo == "PUT") {
+
+} else if($metodo == "DELETE") {
+
+}
+
 ?>
