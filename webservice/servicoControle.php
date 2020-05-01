aa94f6816ec90c7e18b9047cadd11c84161ab647
Bryan <bryanlima200@outlook.com>
2020-04-30 21:56:30 -0300

    Configurado tratamento da requisição HTTP pelo método GET em servicoControle.php

--- webservice/servicoControle.php
+++ webservice/servicoControle.php
@@ -9,6 +9,14 @@ $metodo = $_SERVER['REQUEST_METHOD'];
 
 // Modelo arquitetural REST
 if($metodo == "GET") {
+    $ServicoDao = new ServicoDao();
+    if(isset($_GET['id'])) {
+        $ServicoDao->listarServicoPorId($_GET['id']);
+    } else if(isset($_GET['fkStudioId'])) {
+        $ServicoDao->listarServicoPorStudio($_GET['fkStudioId']);
+    } else {
+        $ServicoDao->listarTodosServicos();
+    }
 
 } else if($metodo == "POST") {
 
