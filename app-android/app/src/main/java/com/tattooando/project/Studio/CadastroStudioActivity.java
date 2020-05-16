02e6347f7848b23a3bf6f9eb825d038b2f136044
Bryan <bryanlima200@outlook.com>
2020-05-15 22:56:36 -0300

    Criado activity CadastroStudio Criado CadastroStudioActivity.java e activity_stu_cadastro.xml

---
+++ app-android/app/src/main/java/com/tattooando/project/Studio/CadastroStudioActivity.java
@@ -0,0 +1,14 @@
+package com.tattooando.project.Studio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroStudioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stu_cadastro);
+    }
+}
