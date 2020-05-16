651d1407359dcc4fb51b04c27fcd7c453a85f1ad
Bryan <bryanlima200@outlook.com>
2020-05-15 23:02:53 -0300

    Criado activity PerfilStudio Criado PerfilStudioActivity.java e activity_stu_perfil.xml

---
+++ app-android/app/src/main/java/com/tattooando/project/Studio/PerfilStudioActivity.java
@@ -0,0 +1,14 @@
+package com.tattooando.project.Studio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PerfilStudioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stu_perfil);
+    }
+}
