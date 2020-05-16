17ac368b8156859db379d08512f2c0a62da6018a
Bryan <bryanlima200@outlook.com>
2020-05-15 23:13:44 -0300

    Import com.tattooando.project.R Importado diretório raiz das activitys do projeto

--- app-android/app/src/main/java/com/tattooando/project/Studio/CadastroStudioActivity.java
+++ app-android/app/src/main/java/com/tattooando/project/Studio/CadastroStudioActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.tattooando.project.R;
+
 public class CadastroStudioActivity extends AppCompatActivity {
 
     @Override
