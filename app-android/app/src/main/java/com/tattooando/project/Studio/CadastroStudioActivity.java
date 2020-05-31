469ff0f9711ea43a843fbdd4c9010d9e2d3e8a15
Bryan <bryanlima200@outlook.com>
2020-05-31 16:33:09 -0300

    Configurado backend da tela Cadastro Studio

--- app-android/app/src/main/java/com/tattooando/project/Studio/CadastroStudioActivity.java
+++ app-android/app/src/main/java/com/tattooando/project/Studio/CadastroStudioActivity.java
@@ -1,16 +1,73 @@
 package com.tattooando.project.Studio;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.tattooando.project.R;
 
 public class CadastroStudioActivity extends AppCompatActivity {
 
+    private Context context = CadastroStudioActivity.this;
+    private Button btnCadastrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stu_cadastro);
+
+        configurarNavBar();
+        inicializarComponentes();
+        eventosClick();
+    }
+
+    private void configurarNavBar() {
+        setTitle("Novo Cadastro");
+        ActionBar actionBar = getSupportActionBar(); // Instancia objeto da BAR
+        actionBar.setDisplayHomeAsUpEnabled(true); // Exibe o ícone
+        actionBar.setHomeButtonEnabled(true); // Habilita o click
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { // Botão adicional na ToolBar
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(context, LoginStudioActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    private void inicializarComponentes() {
+        btnCadastrar = findViewById(R.id.stu_cad_btn_cadastrar);
+    }
+
+    private void eventosClick() {
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, LoginStudioActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(context, LoginStudioActivity.class);
+        startActivity(intent);
+        super.onBackPressed();
+        finish();
     }
 }
