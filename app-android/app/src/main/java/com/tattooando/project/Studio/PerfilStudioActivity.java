c93d31c3de43fde85f76a888fa4ba62e3f8ee187
Bryan <bryanlima200@outlook.com>
2020-05-31 18:12:07 -0300

    Configurado backend da tela Perfil Studio

--- app-android/app/src/main/java/com/tattooando/project/Studio/PerfilStudioActivity.java
+++ app-android/app/src/main/java/com/tattooando/project/Studio/PerfilStudioActivity.java
@@ -1,16 +1,65 @@
 package com.tattooando.project.Studio;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.tattooando.project.R;
 
 public class PerfilStudioActivity extends AppCompatActivity {
 
+    private Context context = PerfilStudioActivity.this;
+    private Button btnAlterar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stu_perfil);
+
+        configurarNavBar();
+        inicializarComponentes();
+        eventosClick();
+    }
+
+    private void configurarNavBar() {
+        setTitle("Perfil");
+        ActionBar actionBar = getSupportActionBar(); // Instancia objeto da BAR
+        actionBar.setDisplayHomeAsUpEnabled(true); // Exibe o ícone
+        actionBar.setHomeButtonEnabled(true); // Habilita o click
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { // Botão adicional na ToolBar
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(context, HomeStudioActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    private void inicializarComponentes() {
+        btnAlterar = findViewById(R.id.stu_per_btn_cadastrar);
+    }
+
+    private void eventosClick() {
+        btnAlterar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, HomeStudioActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
