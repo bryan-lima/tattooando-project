566ae964f2f641d15fd55f2714922309d1a1c58d
Bryan <bryanlima200@outlook.com>
2020-06-01 20:53:54 -0300

    Criado activity Histórico Studio (layout e backend) Histórico de solicitações de agendamento dos clientes

---
+++ app-android/app/src/main/java/com/tattooando/project/Studio/HistoricoStudioActivity.java
@@ -0,0 +1,16 @@
+package com.tattooando.project.Studio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tattooando.project.R;
+
+public class HistoricoStudioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stu_historico);
+    }
+}
