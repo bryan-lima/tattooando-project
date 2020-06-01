753618afe4e9f27651b69957f1247042a1839d0a
Bryan <bryanlima200@outlook.com>
2020-06-01 02:04:09 -0300

    Criado activity Servico Lista Studio (layout e backend)

---
+++ app-android/app/src/main/java/com/tattooando/project/Studio/ServicoListaActivity.java
@@ -0,0 +1,16 @@
+package com.tattooando.project.Studio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tattooando.project.R;
+
+public class ServicoListaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stu_servico_lista);
+    }
+}
