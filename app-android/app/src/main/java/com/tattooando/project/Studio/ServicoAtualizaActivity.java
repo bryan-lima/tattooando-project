d116ef3b2d5bf0b839cb7ccc2926e7ecdabac3c1
Bryan <bryanlima200@outlook.com>
2020-06-01 03:32:32 -0300

    Configurado backend da tela Atualizar Serviço

--- app-android/app/src/main/java/com/tattooando/project/Studio/ServicoAtualizaActivity.java
+++ app-android/app/src/main/java/com/tattooando/project/Studio/ServicoAtualizaActivity.java
@@ -1,16 +1,73 @@
 package com.tattooando.project.Studio;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.tattooando.project.R;
 
 public class ServicoAtualizaActivity extends AppCompatActivity {
 
+    private Context context = ServicoAtualizaActivity.this;
+    private Button btnAtualizar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stu_servico_atualiza);
+
+        configurarNavBar();
+        inicializarComponentes();
+        eventosClick();
+    }
+
+    private void configurarNavBar() {
+        setTitle("Atualizar Serviço");
+        ActionBar actionBar = getSupportActionBar(); // Instancia objeto da BAR
+        actionBar.setDisplayHomeAsUpEnabled(true); // Exibe o ícone
+        actionBar.setHomeButtonEnabled(true); // Habilita o click
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { //Botão adicional na ToolBar
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(context, HomeStudioActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(context, HomeStudioActivity.class);
+        startActivity(intent);
+        super.onBackPressed();
+        finish();
+    }
+
+    private void inicializarComponentes() {
+        btnAtualizar = findViewById(R.id.stu_ser_upd_btn_atualizar);
+    }
+
+    private void eventosClick() {
+        btnAtualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, HomeStudioActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
