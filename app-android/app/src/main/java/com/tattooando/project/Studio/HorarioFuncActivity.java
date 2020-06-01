749ea2ae6196ca9e8bf9555114b3d9f68ec1a343
Bryan <bryanlima200@outlook.com>
2020-06-01 01:57:45 -0300

    Criado activity Horário Funcionamento Studio (layout e backend)

---
+++ app-android/app/src/main/java/com/tattooando/project/Studio/HorarioFuncActivity.java
@@ -0,0 +1,16 @@
+package com.tattooando.project.Studio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tattooando.project.R;
+
+public class HorarioFuncActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horario_func);
+    }
+}
