5a0059f19dbcd2e64c1b450da899fdd71d5e3b36
Bryan <bryanlima200@outlook.com>
2020-06-01 21:45:59 -0300

    Configurado navbar, título e botão voltar via código (backend) da tela Listar Serviços

--- app-android/app/src/main/java/com/tattooando/project/Studio/ServicoListaActivity.java
+++ app-android/app/src/main/java/com/tattooando/project/Studio/ServicoListaActivity.java
@@ -1,16 +1,45 @@
 package com.tattooando.project.Studio;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.tattooando.project.R;
 
 public class ServicoListaActivity extends AppCompatActivity {
 
+    private Context context = ServicoListaActivity.this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stu_servico_lista);
+
+        configurarNavBar();
+    }
+
+    private void configurarNavBar() {
+        setTitle("Seus Serviços");
+        ActionBar actionBar = getSupportActionBar(); // Instancia objeto da BAR
+        actionBar.setDisplayHomeAsUpEnabled(true); // Exibe o ícone
+        actionBar.setHomeButtonEnabled(true); // Habilita o click
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { // Botão adicional na ToolBar
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(context, HomeStudioActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
