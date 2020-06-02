6774a1854ad61c4e073b1cbc15d553648972fc6b
Bryan <bryanlima200@outlook.com>
2020-06-01 21:45:01 -0300

    Configurado navbar, título e botão voltar via código (backend) da tela Historico (fluxo Cliente)

--- app-android/app/src/main/java/com/tattooando/project/Cliente/HistoricoClienteActivity.java
+++ app-android/app/src/main/java/com/tattooando/project/Cliente/HistoricoClienteActivity.java
@@ -1,16 +1,45 @@
 package com.tattooando.project.Cliente;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.tattooando.project.R;
 
 public class HistoricoClienteActivity extends AppCompatActivity {
 
+    private Context context = HistoricoClienteActivity.this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cli_historico);
+
+        configurarNavBar();
+    }
+
+    private void configurarNavBar() {
+        setTitle("Histórico");
+        ActionBar actionBar = getSupportActionBar(); // Instancia objeto da BAR
+        actionBar.setDisplayHomeAsUpEnabled(true); // Exibe o ícone
+        actionBar.setHomeButtonEnabled(true); // Habilita o click
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { // Botão adicional na ToolBar
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(context, HomeClienteActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
